496b9454da5edf7deb27a9814c1e8867a3297c57
maks2707a <aleshkinmaksim2009@gmail.com>
2025-07-07 15:13:35 +0300

    added coordinates, confidence and object class
    added coordinates, confidence and object class. photos doesn't saving now and boxes data returning from detecton function

--- cam+yolo.py
+++ cam+yolo.py
@@ -3,13 +3,14 @@ import os
 import time
 from datetime import datetime
 from ultralytics import YOLO
+import pandas as pd
 
 
-desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+'''esktop = os.path.join(os.path.expanduser('~'), 'Desktop')
 
 #
 save_folder = os.path.join(desktop, 'Camera_Shots')
-os.makedirs(save_folder, exist_ok=True)
+os.makedirs(save_folder, exist_ok=True)'''
 
 
 camera = cv2.VideoCapture(0)
@@ -19,17 +20,42 @@ if 0 == camera.isOpened():
     print("Камера не подключена")
     exit()
 class model:
-    def __init__(self, src:str, conf1=float, name=str):
-        self.src=src
+    def __init__(self, conf1=float, name=str):
         self.conf1=conf1
         self.name=name
     def load_model(self):
         global model
         model = YOLO(self.name)
-    def res(self,sorc):
-        results = model.predict(source=sorc, conf=self.conf1, save=True, show=True)
+    def res(self,src):
+        results = model.predict(source=src, conf=self.conf1, save=False, show=True)
+        boxes=results[0].boxes
+        for box in boxes:
+            x1, y1 ,x2, y2 = box.xyxy[0].tolist()
+            width = x2 - x1
+            height = y2 - y1
+            area = width * height
+            center_x = (x1 + x2) / 2
+            center_y = (y1 + y2) / 2
+            class_id=int(box.cls)
+            class_name=model.names[class_id]
+            confidence=float(box.conf)
+            data=[]
+            data.append({"class id": class_id,
+                         "class name": class_name,
+                         "confidence": confidence,
+                         "x1": x1,
+                         "y1": y1,
+                         "x2": x2,
+                         'y2': y2,
+                         "width": width,
+                         'height': height,
+                         "area": area,
+                         "center_x": center_x,
+                         "center_y": center_y})
+            df = pd.DataFrame(data)
+            return df
 
-test=model('20250705_145850.jpg', 0.5, 'yolo11x.pt')
+test=model(0.5, 'yolo11x.pt')
 test.load_model()
 
 print("Камера работает. Нажми Q, чтобы выйти")
@@ -37,22 +63,23 @@ while True:
     t = time.time()
 
 
-    a = []
-    a = camera.read()
+    image = []
+    image = camera.read()
 
-    if 0 == a[0]:
+    if 0 == image[0]:
         print("Не могу получить кадр...")
         break
-    cv2.imshow("Live Camera", a[1])
-    if a[0]:
-        # Создаем имя файла с текущей датой и временем
+    cv2.imshow("Live Camera", image[1])
+    if image[0]:
+        '''# Создаем имя файла с текущей датой и временем
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         filename = f"photo_{timestamp}.jpg"
         filepath = os.path.join(save_folder, filename)
 
         # Сохраняем изображение
         cv2.imwrite(filepath, a[1])
-        print(f"Снимок сохранен: {filepath}")
+        print(f"Снимок сохранен: {filepath}")'''
+        test.res(image[1])
 
     else:
         print("Не удалось сделать снимок")
@@ -60,6 +87,6 @@ while True:
     if cv2.waitKey(1) == ord('q') & 0xFF == ord('q'):
         break
     time.sleep(0.5)
-    test.res(filepath)
+
 camera.release()
 cv2.destroyAllWindows()
